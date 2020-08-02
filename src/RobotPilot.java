4679dc76985ee207e632d014e8fb1a3da4620d1e
Leonard Oeding <leo.oeding@gmail.com>
2020-08-02 17:08:56 +0200

    Added rotate() method

--- src/RobotPilot.java
+++ src/RobotPilot.java
@@ -21,7 +21,8 @@ public class RobotPilot {
 
 	}
 
-	public RobotPilot(Port leftMotor, Port rightMotor, float wheelDiameter, float chassisWidth, boolean inverted){
+	public RobotPilot(Port leftMotor, Port rightMotor, float wheelDiameter, float chassisWidth, boolean inverted) {
+//		Set inverted to true if your robot is moving backwards when traveling forward and vice versa
 		this.motor1 = new EV3LargeRegulatedMotor(leftMotor);
 		this.motor2 = new EV3LargeRegulatedMotor(rightMotor);
 		motor1.setSpeed(motor1.getMaxSpeed());
@@ -33,6 +34,7 @@ public class RobotPilot {
 	}
 
 	public void travel(float distance) {
+//		moves the robot forward
 		float degreesToTravel = distance / ((float) ((Math.PI * wheelDiameter) / 360)); 
 		if (inverted) {
 			degreesToTravel = -degreesToTravel;
@@ -43,6 +45,7 @@ public class RobotPilot {
 	}
 	
 	public void travel(float distance, boolean immediateReturn) {
+//		moves the robot forward
 		float degreesToTravel = distance / ((float) ((Math.PI * wheelDiameter) / 360)); 
 		if (inverted) {
 			degreesToTravel = -degreesToTravel;
@@ -57,6 +60,7 @@ public class RobotPilot {
 	}
 	
 	public void setRotateSpeed(float rotateSpeed) {
+//		Sets the speed the robot uses when rotating
 		this.rotateSpeed = rotateSpeed;
 	}
 	
@@ -98,6 +102,7 @@ public class RobotPilot {
 	}
 	
 	public void moveLeftMotor(float distance) {
+//		Same as travel(distance) but onlz moves one motor
 		float degreesToTravel = distance / ((float) ((Math.PI * wheelDiameter) / 360));
 		if (inverted) {
 			degreesToTravel = -degreesToTravel;
@@ -106,6 +111,7 @@ public class RobotPilot {
 	}
 	
 	public void moveLeftMotor(float distance, boolean immediateReturn) {
+//		Same as travel(distance) but onlz moves one motor
 		float degreesToTravel = distance / ((float) ((Math.PI * wheelDiameter) / 360));
 		if (inverted) {
 			degreesToTravel = -degreesToTravel;
@@ -114,6 +120,7 @@ public class RobotPilot {
 	}
 	
 	public void moveRightMotor(float distance) {
+//		Same as travel(distance) but onlz moves one motor
 		float degreesToTravel = distance / ((float) ((Math.PI * wheelDiameter) / 360));
 		if (inverted) {
 			degreesToTravel = -degreesToTravel;
@@ -122,6 +129,7 @@ public class RobotPilot {
 	}
 	
 	public void moveRightMotor(float distance, boolean immediateReturn) {
+//		Same as travel(distance) but onlz moves one motor
 		float degreesToTravel = distance / ((float) ((Math.PI * wheelDiameter) / 360));
 		if (inverted) {
 			degreesToTravel = -degreesToTravel;
@@ -195,12 +203,14 @@ public class RobotPilot {
 	}
 	
 	public void setSpeed(float speed) {
+//		Sets the speed of the robot
 		this.speed = speed;
 		motor1.setSpeed(speed);
 		motor2.setSpeed(speed);
 	}
 	
 	public void startRotate(boolean right) {
+//		Starts a rotating motion
 		motor1.setSpeed(rotateSpeed);
 		motor2.setSpeed(rotateSpeed);
 		Delay.msDelay(25);
@@ -225,6 +235,7 @@ public class RobotPilot {
 	}
 	
 	public float getMaxSpeed() {
+//		returns the maximal speed the robot can achieve
 		float mot1speed = motor1.getMaxSpeed();
 		float mot2speed = motor2.getMaxSpeed();
 		return Math.min(mot1speed, mot2speed);
@@ -232,7 +243,79 @@ public class RobotPilot {
 	}
 	
 	public void setAcceleration(int acceleration) {
+//		Sets the Linear acceleration of the robot
 		motor1.setAcceleration(acceleration);
 		motor2.setAcceleration(acceleration);
 	}
+	
+	public void rotate(float angle) {
+//		The friction value is 1 by default. You should change it depending on your robot`s friction when rotating. To find out the right value, just try and test until the robot turns the right angle. The higher the value, the higher the friction.
+		float friction = 1;
+//		Set the speed of motors to rotateSpeed
+		motor1.setSpeed(rotateSpeed);
+		motor2.setSpeed(rotateSpeed);
+		Delay.msDelay(25);
+//		Perform calculations
+		double circleValue = chassisWidth * Math.PI; 
+		float distanceToTravel = (float) (circleValue / (360 / angle));
+		int degreesToTravel = (int) (distanceToTravel / ((float) ((Math.PI * wheelDiameter) / 360))); 
+//		Rotate the motors
+		motor1.rotate((int) (degreesToTravel * friction), true);
+		motor2.rotate((int) (-degreesToTravel * friction));
+//		Set the speed of motors back to speed
+		setSpeed(speed);
+	}
+	
+	public void rotate(float angle, float friction) {
+//		The friction value is 1 by default. You should change it depending on your robot`s friction when rotating. To find out the right value, just try and test until the robot turns the right angle. The higher the value, the higher the friction.
+//		Set the speed of motors to rotateSpeed
+		motor1.setSpeed(rotateSpeed);
+		motor2.setSpeed(rotateSpeed);
+		Delay.msDelay(25);
+//		Perform calculations
+		double circleValue = chassisWidth * Math.PI; 
+		float distanceToTravel = (float) (circleValue / (360 / angle));
+		int degreesToTravel = (int) (distanceToTravel / ((float) ((Math.PI * wheelDiameter) / 360))); 
+//		Rotate the motors
+		motor1.rotate((int) (degreesToTravel * friction), true);
+		motor2.rotate((int) (-degreesToTravel * friction));
+//		Set the speed of motors back to speed
+		setSpeed(speed);
+	}
+	
+	public void rotate(float angle, boolean immediateReturn) {
+//		The friction value is 1 by default. You should change it depending on your robot`s friction when rotating. To find out the right value, just try and test until the robot turns the right angle. The higher the value, the higher the friction.
+		float friction = 1;
+//		Set the speed of motors to rotateSpeed
+		motor1.setSpeed(rotateSpeed);
+		motor2.setSpeed(rotateSpeed);
+		Delay.msDelay(25);
+//		Perform calculations
+		double circleValue = chassisWidth * Math.PI; 
+		float distanceToTravel = (float) (circleValue / (360 / angle));
+		int degreesToTravel = (int) (distanceToTravel / ((float) ((Math.PI * wheelDiameter) / 360))); 
+//		Rotate the motors
+		motor1.rotate((int) (degreesToTravel * friction), true);
+		motor2.rotate((int) (-degreesToTravel * friction), immediateReturn);
+//		Set the speed of motors back to speed
+		setSpeed(speed);
+	}
+	
+	public void rotate(float angle, float friction, boolean immediateReturn) {
+//		The friction value is 1 by default. You should change it depending on your robot`s friction when rotating. To find out the right value, just try and test until the robot turns the right angle. The higher the value, the higher the friction.
+//		Set the speed of motors to rotateSpeed
+		motor1.setSpeed(rotateSpeed);
+		motor2.setSpeed(rotateSpeed);
+		Delay.msDelay(25);
+//		Perform calculations
+		double circleValue = chassisWidth * Math.PI; 
+		float distanceToTravel = (float) (circleValue / (360 / angle));
+		int degreesToTravel = (int) (distanceToTravel / ((float) ((Math.PI * wheelDiameter) / 360))); 
+//		Rotate the motors
+		motor1.rotate((int) (degreesToTravel * friction), true);
+		motor2.rotate((int) (-degreesToTravel * friction), immediateReturn);
+//		Set the speed of motors back to speed
+		setSpeed(speed);
+	}
+	
 }
